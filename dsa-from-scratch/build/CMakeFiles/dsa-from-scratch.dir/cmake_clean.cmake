1b290d0c87c484a452af3b04e3e904a4dda47f3b
Heathcliff <2826190541@qq.com>
2024-08-12 10:50:58 +0800

    basic

---
+++ dsa-from-scratch/build/CMakeFiles/dsa-from-scratch.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dsa-from-scratch.dir/src/main.cpp.obj"
+  "CMakeFiles/dsa-from-scratch.dir/src/main.cpp.obj.d"
+  "dsa-from-scratch.exe"
+  "dsa-from-scratch.exe.manifest"
+  "dsa-from-scratch.pdb"
+  "libdsa-from-scratch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dsa-from-scratch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ dsa-from-scratch/build/CMakeFiles/dsa-from-scratch.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for dsa-from-scratch.
+# This may be replaced when dependencies are built.
